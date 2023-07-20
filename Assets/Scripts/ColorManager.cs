b97ad6544016dc00a3496d5754b33ecb14779f3a
matzaj1 <48223772+matzaj1@users.noreply.github.com>
2023-07-20 21:12:50 +0200

    Update ColorManager.cs

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -11,10 +11,20 @@ public class ColorManager : MonoBehaviour
     public Color RareColor;
     public Color LegendaryColor;
 
+    public Color AdditionColorTheme;
+    public Color SubtractionColorTheme;
+    public Color MultiplicationColorTheme;
+    public Color DivisionColorTheme;
+
     public Sprite clockImage;
     public Sprite attackImage;
     public Sprite defenseImage;
     public Sprite extraImage;
+
+    public Texture additionImage;
+    public Texture subtractionImage;
+    public Texture multiplicationImage;
+    public Texture divisionImage;
 
     private void Awake()
     {
@@ -52,5 +62,30 @@ public class ColorManager : MonoBehaviour
             Rarity.Legendary => LegendaryColor,
             _ => NoneColor,
         };
+    }
+
+    public Color GetColorByMathOperationType(MathOperationType mathOperationType)
+    {
+        Color color;
+        switch (mathOperationType)
+        {
+            case MathOperationType.Addition:
+                color = AdditionColorTheme;
+                break;
+            case MathOperationType.Subtraction:
+                color = SubtractionColorTheme;
+                break;
+            case MathOperationType.Multiplication:
+                color = MultiplicationColorTheme;
+                break;
+            case MathOperationType.Division:
+                color = DivisionColorTheme;
+                break;
+            default:
+                color = AdditionColorTheme;
+                break;                
+        }
+
+        return color;
     }
 }
