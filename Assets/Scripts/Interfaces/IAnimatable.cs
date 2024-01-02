051c552f35b337a0d97498d965845dd968d5afc3
matzaj1 <48223772+matzaj1@users.noreply.github.com>
2024-01-02 14:52:28 +0100

    Add IAnimatable

---
+++ Assets/Scripts/Interfaces/IAnimatable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public interface IAnimatable
+{
+    void Show();
+    void Collapse();
+    void Toggle();
+    IEnumerator Animate();
+    IEnumerator SuspendButtons(float time, List<AnimatedButton> buttons);
+}
